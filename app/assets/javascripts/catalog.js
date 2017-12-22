1c253c9311ff1f5b19079545aae9e9a51ffa3b16
David Grove Jørgensen <dgj@kb.dk>
2017-12-22 14:38:37 +0100

    exact matches in text searches

--- app/assets/javascripts/catalog.js
+++ app/assets/javascripts/catalog.js
@@ -1,22 +1,23 @@
 // Called in the _index_default.html to populate the index partial with links of text searches
 // If the matxhes are more than 3, it creates a button that triggers a modal with all the matches/links
-function index_work_search(id, modal_selector, modal_body_selector,target_selector, text_label_id, q){
+function index_work_search(id, modal_selector, modal_body_selector,target_selector, text_label_id, q, match){
     id = encodeURIComponent(id);
     if (!q.trim()){
         $(text_label_id).hide();
     }else{
         $.ajax({
             type: 'GET',
-            url: '/text.json?search_field=leaf&rows=200&sort=position_isi+asc&q='+q+'&workid='+id,
+            url: '/text.json?search_field=leaf&rows=200&sort=position_isi+asc&q='+q+'&workid='+id+'&match='+match,
             datatype: 'json',
             success: function(data) {
                 $(target_selector).empty();
                 docs = data.data;
+                hl_field = (match == 'phrase')?'text_tsim':'text_tesim';
                 matches_num = data.meta.pages.total_count;
                 if (matches_num>0) {
                     $(target_selector).append('<div id="results-header"><p>'+matches_num+' match</p></div>');
                     for (var i= 0; i in docs && i<3; i++) {
-                        $(target_selector).append('<p><a href="/text/' + id + '#' + docs[i].attributes.xmlid_ssi + '">' + docs[i].highlighting[docs[i].id].text_tesim.join("...") + '</a></br>Side: ' + docs[i].attributes.page_ssi + '</p>');                        // }
+                        $(target_selector).append('<p><a href="/text/' + id + '#' + docs[i].attributes.xmlid_ssi + '">' + docs[i].highlighting[docs[i].id][hl_field].join("...") + '</a></br>Side: ' + docs[i].attributes.page_ssi + '</p>');                        // }
                     }
                 }if (matches_num>3){
                     var btn = document.createElement("BUTTON");
@@ -29,7 +30,7 @@ function index_work_search(id, modal_selector, modal_body_selector,target_select
                         $(modal_selector).modal();
                     });
                     for (var i= 0; i in docs ; i++) {
-                        $(modal_body_selector).append('<p><a href="/text/' + id + '#' + docs[i].attributes.xmlid_ssi + '">' + docs[i].highlighting[docs[i].id].text_tesim.join("...") + '</a></br>Side: ' + docs[i].attributes.page_ssi + '</p>');                        // }
+                        $(modal_body_selector).append('<p><a href="/text/' + id + '#' + docs[i].attributes.xmlid_ssi + '">' + docs[i].highlighting[docs[i].id][hl_field].join("...") + '</a></br>Side: ' + docs[i].attributes.page_ssi + '</p>');                        // }
                         }
                 }if(matches_num==0){$(text_label_id).hide();} // If the number of matches is 0, hide the label
             }
