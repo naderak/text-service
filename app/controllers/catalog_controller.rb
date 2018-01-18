10baffab445108ab1f847a06d6496a964c63b9b7
bimo <zahramousavidk@gmail.com>
2018-01-18 11:29:09 +0100

    Changed the label from udgave to anvendt udgave

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -90,7 +90,7 @@ class CatalogController < ApplicationController
 
     # Work show fields
     config.add_show_field 'author_id_ssi', :label => 'Forfatter', helper_method: :author_link, itemprop: :author
-    config.add_show_field 'volume_title_tesim', :label => 'Udgave', helper_method: :show_volume, itemprop: :isPartOf #, unless: proc { |_context, _field_config, doc | doc.id == doc['volume_id_ssi'] }
+    config.add_show_field 'volume_title_tesim', :label => 'Anvendt udgave', helper_method: :show_volume, itemprop: :isPartOf #, unless: proc { |_context, _field_config, doc | doc.id == doc['volume_id_ssi'] }
     config.add_show_field 'volume_title_ssi', :label => 'Citér', helper_method: :citation #, unless: proc { |_context, _field_config, doc | doc.id == doc['volume_id_ssi'] }
     #config.add_show_field 'publisher_tesim', :label => 'Udgiver', unless: proc { |_context, _field_config, doc | doc['cat_ssi'] == 'volume' }
     #config.add_show_field 'place_published_tesim', :label => 'Udgivelsessted'
